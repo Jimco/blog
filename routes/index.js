fde1188e90247d2f6d8366fc1df3c512370c1880
Jiancong Xie <jimco@color7.net>
2013-12-20 17:17:25 +0800

    add user&article page model

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ module.exports = function(app){
 
   // 首页
   app.get('/', function (req, res) {
-    Post.get(null, function(err, posts){
+    Post.getAll(null, function(err, posts){
       if(err) posts = [];
 
       res.render('index', {
@@ -51,6 +51,52 @@ module.exports = function(app){
 
       req.flash('success', '发布成功！');
       res.redirect('/');
+    });
+  });
+
+
+  // 用户页面
+  app.get('/u/:name', function(req, res){
+    User.get(req.params.name, function(err, user){
+      if(!user){
+        req.flash('error', '用户不存在！');
+        return res.redirect('/');
+      }
+
+      // 查询并返回该用户的所有文章
+      Post.getAll(user.name, function(err, posts){
+        if(err){
+          req.flash('error', err);
+          return res.redirect('/');
+        }
+
+        res.render('user', {
+          title: user.name,
+          posts: posts,
+          user: req.session.user,
+          success: req.flash('success').toString(),
+          error: req.flash('error').toString()
+        })
+      });
+    });
+  });
+
+
+  // 文章页面
+  app.get('/u/:name/:day/:title', function(req, res){
+    Post.getOne(req.params.name, req.params.day, req.params.title, function(err, post){
+      if(err){
+        req.flash('error', err);
+        return res.redirect('/');
+      }
+
+      res.render('article', {
+        title: req.params.title,
+        post: post,
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+      });
     });
   });
 
