3a039e8982e06b8cbe8cebba1b3b457d75db295b
Jiancong Xie <jimco@color7.net>
2013-12-26 14:43:42 +0800

    add page views and comment count

--- models/post.js
+++ models/post.js
@@ -28,7 +28,8 @@ Post.prototype.save = function(callback){
       title: this.title,
       tags: this.tags,
       post: this.post,
-      comments: []
+      comments: [],
+      pv: 0
     };
 
   // 打开数据库
@@ -102,10 +103,23 @@ Post.getOne = function(name, day, title, callback){
         "time.day": day,
         "title": title
       }, function(err, doc){
-        mongodb.close();
-        if(err) return callback(err);
-        // doc.post = markdown.toHTML(doc.post);
+        if(err){
+          mongodb.close();
+          return callback(err);
+        }
         if(doc){
+          // pv+1
+          collection.update({
+            "name": name,
+            "time.day": day,
+            "title": title
+          }, {
+            $inc: {"pv": 1}
+          }, function(err){
+            mongodb.close();
+            if(err) return callback(err);
+          });
+
           doc.post = markdown.toHTML(doc.post);
           doc.comments.forEach(function(comment){
             comment.content = markdown.toHTML(comment.content);
