49cd67f3430f1339190fef9f04d666e62f337da4
Jiancong Xie <jimco@color7.net>
2014-01-06 15:31:27 +0800

    time formatting

--- public/js/src/timer.js
+++ public/js/src/timer.js
@@ -7,15 +7,16 @@ define(function(require, exports, module){
   return function(ele){
     var timerTpl = '<span class="time_section"><span class="time_amount"><%= days %></span><br><%= months %></span><span class="time_section"><span class="time_amount"><%= hours %></span><br>Hour</span><span class="time_section"><span class="time_amount"><%= minutes%></span><br>Minute</span><span class="time_section"><span class="time_amount"><%= seconds %></span><br>Second</span>';
 
-    setInterval(function() {
+    setInterval(function(){
       var date = new Date()
+        , zeroComplet = function(s){ return s < 10 ? '0' + s : s; }
         , aMonth = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
         , data = {
-          days: date.getDate(),
+          days: zeroComplet(date.getDate()),
           months: aMonth[date.getMonth()],
-          seconds: date.getSeconds(),
-          minutes: date.getMinutes(),
-          hours: date.getHours()
+          seconds: zeroComplet(date.getSeconds()),
+          minutes: zeroComplet(date.getMinutes()),
+          hours: zeroComplet(date.getHours())
         };
 
       ele.innerHTML = util.template(timerTpl, data);
