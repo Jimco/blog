6cbb2e715821a3c28e8080be0d620c3f0a72a8fe
Jiancong Xie <jimco@color7.net>
2014-01-29 14:35:45 +0800

    modify static config file and build it

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,9 +6,9 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     // 清除临时文件
     clean: {
-      main: ['static/js/.tmp*']        // 需要清除的文件夹名称
+      main: ['public/js/.tmp*']        // 需要清除的文件夹名称
     },
-    // 提取依赖
+    // cmd 模块提取依赖
     transport: {
       options: {
         path: ['js'],           // 模块的 id 格式
@@ -20,13 +20,13 @@ module.exports = function(grunt) {
         },
         files: [{
           expand: true,
-          cwd: 'static/js/src',              // 相对路径
+          cwd: 'public/js/src',              // 相对路径
           src: '*.js',                       // 需要提取依赖的文件
-          dest: 'static/js/.tmp1'            // 提取后存放的临时文件
+          dest: 'public/js/.tmp1'            // 提取后存放的临时文件
         }]
       }
     },
-    // 合并
+    // cmd 模块合并
     concat: {
       options: {
         include: 'relative'  // 合并模式(relative: 只合并相对路径的模块, all: 合并所有模块)
@@ -34,21 +34,21 @@ module.exports = function(grunt) {
       js: {
         files: [{
           expand: true,
-          cwd: 'static/js/.tmp1',         // 需要合并的文件夹路径
+          cwd: 'public/js/.tmp1',         // 需要合并的文件夹路径
           src: '*.js',                    // 需要合并的文件
           filter: function(filepath) {
             return !/-debug\.js$/.test(filepath); // 筛选非 -dubug.js 结尾的文件
           },
-          dest: 'static/js/.tmp2',        // 合并后的存放路径
+          dest: 'public/js/.tmp2',        // 合并后的存放路径
           ext: '.js'                      // 合并后的扩展名
         }]
       },
       common: {
         files: [{
           expand: true,
-          cwd: 'static/lib',
-          src: '*.js',
-          dest: 'static/js/dist/lib'
+          cwd: 'public/js',
+          src: ['jquery-1.8.3.min.js', 'sea.js'],
+          dest: 'public/js/dist/lib'
         }]
       }
     },
@@ -83,10 +83,17 @@ module.exports = function(grunt) {
       js: {
         files: [{
           expand: true,
-          cwd: 'static/js/.tmp2',       // 需要压缩的文件夹路径
+          cwd: 'public/js/.tmp2',       // 需要压缩的文件夹路径
           src: '*.js',                  // 需要压缩的文件
-          dest: 'static/js/dist',       // 压缩后的存放路径
+          dest: 'public/js/dist',       // 压缩后的存放路径
           ext: '.js'                    // 压缩后的扩展名
+        }]
+      },
+      common: {
+        files:[{
+          cwd: 'public/js/lib',                 // 需要压缩的文件夹路径
+          src: 'common.js',                     // 需要压缩的文件
+          dest: 'public/js/dist/common'  // 压缩后的存放路径
         }]
       }
     }
